3985673f323464719c97861862831d4b3a69c8b6
Johirul Islam <johirbuet@gmail.com>
2017-07-04 11:37:46 -0500

    fix bug of add remove while changin orientation

--- app/src/main/java/com/johir/useraction/addingfragmentbyjava/MainActivity.java
+++ app/src/main/java/com/johir/useraction/addingfragmentbyjava/MainActivity.java
@@ -1,27 +1,28 @@
 package com.johir.useraction.addingfragmentbyjava;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    SimpleFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fragment = new SimpleFragment();
 
     }
 
     public void addFragment(View v){
+        SimpleFragment fragment = new SimpleFragment();
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_simple,fragment)
                 .commit();
     }
 
     public void removeFragment(View v){
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_simple);
         if(fragment != null){
             getSupportFragmentManager().beginTransaction()
                     .remove(fragment)

de88badfc6a175bf78f0647cf88a539bc31e06e1
Johirul Islam <johirbuet@gmail.com>
2017-07-04 11:35:22 -0500

    dynamically adding removing

--- app/src/main/java/com/johir/useraction/addingfragmentbyjava/MainActivity.java
+++ app/src/main/java/com/johir/useraction/addingfragmentbyjava/MainActivity.java
@@ -2,16 +2,30 @@ package com.johir.useraction.addingfragmentbyjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    SimpleFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleFragment fragment = new SimpleFragment();
+        fragment = new SimpleFragment();
+
+    }
+
+    public void addFragment(View v){
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fragment_simple,fragment)
                 .commit();
+    }
+
+    public void removeFragment(View v){
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
     }
 }
